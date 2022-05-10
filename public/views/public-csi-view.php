d1cd5f33bd5c368fe34a64f1d5a37d82f4687477
Windesheim <winnovator.windesheim@gmail.com>
2022-05-10 23:24:31 +0200

    Added loading spinner

--- public/views/public-csi-view.php
+++ public/views/public-csi-view.php
@@ -10,7 +10,7 @@ class PublicCaseStudyIndexView extends PublicCaseStudyIndexController {
         $output = '';
         $output .= '<div id="csi-filter-button-container"><button id="csi-filter-button" type="button">Filter</button></div>';
         $output .= '<div id="csi-grid-container">';
-        $output .= '<div id="csi-content" class="csi-content"></div>';
+        $output .= '<div id="csi-content" class="csi-content"><div class="csi-public-loader"></div></div>';
         $output .= '<div id="csi-public-info-modal-container"></div>';
         $output .= '<div id="csi-side-panel"></div>';
         $output .= '<div id="csi-pagination"><div id="csi-pagination-bttns">';
